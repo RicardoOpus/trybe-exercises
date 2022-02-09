5cc4e7dde622e344cd2bbdb5534bf93266e30fab
RicardoOpus <ricardoopus125@gmail.com>
2022-02-09 10:58:52 -0300

    envia exercicio de fixacao 5.1

---
+++ Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/01: JavaScript - DOM e seletores/script.js
@@ -0,0 +1,17 @@
+document.querySelector("#header-container").style.backgroundColor = "#00B069";
+
+document.querySelector("#container").style.backgroundColor = "#F1F1F1";
+
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "#FF9F84"
+
+document.querySelectorAll(".no-emergency-tasks")[0].style.backgroundColor = "#F9DB5E";
+
+document.getElementsByTagName("footer")[0].style.backgroundColor = "#036B52";
+
+document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor = "#A500F3";
+
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = "#A500F3";
+
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = "#003533";
+
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = "#003533";
